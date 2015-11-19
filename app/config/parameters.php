b11cf0f36e2efab3d77bc5df207642a8e2bd06a7
Tom Brendlé <tom.brendle@hotmail.fr>
2015-11-18 21:44:56 -0800

    'h'

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -1,13 +1,11 @@
 <?php 
-
 $db = parse_url($container->getParameter('DATABASE_URL')?:'mysql://root@localhost/symfony');
 if($db['scheme']=='postgres')
 	$container->setParameter('database_driver', 'pdo_pgsql' );
 else
 	$container->setParameter('database_driver', 'pdo_mysql' );
-var_dump($db);
 $container->setParameter('database_user', (isset($db['user']) ? $db['user'] : 'root'));
 $container->setParameter('database_password', (isset($db['pass']) ? $db['pass'] : null));
-$container->setParameter('database_host', (isset($db['hotst']) ? $db['host'] : 'localhost'));
+$container->setParameter('database_host', (isset($db['host']) ? $db['host'] : 'localhost'));
 if(isset($db['port'])) $container->setParameter('database_port', $db['port']);
 $container->setParameter('database_name', substr($db['path'], 1));
